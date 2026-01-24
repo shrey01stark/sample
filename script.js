633ad369e4d7ebe6278d98dbd72edf11051f85c1
Shreyansh Tiwari <125348065+shrey01stark@users.noreply.github.com>
2026-01-24 17:16:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", function() {
+const preloader = document.getElementById("preloader");
+
+
+setTimeout(function() {
+    // Hide the loader
+    preloader.classList.add("loader-hidden");
+    // Re-enable scrolling
+    document.body.style.overflow = "auto";
+}, 2000); // 3 seconds
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuTrigger = document.getElementById('menuTrigger');
+    const mobHeader = document.querySelector('.mob-header');
+
+    menuTrigger.addEventListener('click', () => {
+        // Toggle the Cross Icon
+        menuTrigger.classList.toggle('active');
+        
+        // Toggle the Div visibility (0 to 100%)
+        mobHeader.classList.toggle('open');
+    });
+
+    // Optional: Close menu when a link is clicked
+    const mobLinks = document.querySelectorAll('.mob-a');
+    mobLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuTrigger.classList.remove('active');
+            mobHeader.classList.remove('open');
+        });
+    });
+});
+
+
+const header = document.querySelector('header');
+const showAnim = gsap.from(header, { 
+  yPercent: -100,
+  paused: true,
+  duration: 0.3
+}).progress(1);
+
+ScrollTrigger.create({
+  start: "top top",
+  end: 99999,
+  onUpdate: (self) => {
+    // self.direction is 1 when scrolling down, -1 when scrolling up
+    if (self.direction === -1) {
+      showAnim.play(); // Scrolling up: Show header
+    } else {
+      showAnim.reverse(); // Scrolling down: Hide header
+    }
+  }
+});
+
